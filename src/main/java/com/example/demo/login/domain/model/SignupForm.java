d88e93b03c8df2659af68de9f734a8d1a4676494
yuhei kobayashi <t-yuhkobayashi@yahoo-corp.jp>
2019-10-17 15:06:37 +0900

    コミット漏れ

--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -1,15 +1,7 @@
 package com.example.demo.login.domain.model;
 
 import java.util.Date;
-
-import javax.validation.constraints.AssertFalse;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
+import javax.validation.constraints.*;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
