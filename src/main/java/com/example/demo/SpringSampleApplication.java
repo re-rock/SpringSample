0cf3a04c71cbac33541dfc8e6662734502cde27a
myuser <baysik734@gmail.com>
2019-11-22 18:57:17 +0900

    test from feature-b

--- src/main/java/com/example/demo/SpringSampleApplication.java
+++ src/main/java/com/example/demo/SpringSampleApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSampleApplication {
-
+    
     public static void main(String[] args) {
+        
+        // マージテスト2
+        System.out.println("feature-bのソースです。");
         SpringApplication.run(SpringSampleApplication.class, args);
     }
-
+    
 }
